36ebc9efa25955ebb01fb7371dfe8b00a5a81aec
Enrique Ceballos <enrique_cpl12@hotmail.com>
2022-05-17 23:39:36 -0300

    Desafio numero 5

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const routes = require(`./routes/routesNames`);
 const multer = require("multer");
+const handlebars = require("express-handlebars");
+const ejs = require(`ejs`);
+const fs = require(`fs`);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -17,16 +20,53 @@ app.use(
     storage,
   }).single("thumbnail")
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-app.use("/", routes);
-app.use("/api/productos", routes);
-app.use("/api/productos/:id", routes);
 
-app.get("/api", (req, res) => {
-  return res.json();
+// HANDLEBARS
+app.set(`views`, `./views`);
+app.set(`view engine`, `hbs`);
+
+app.engine(`hbs`, handlebars);
+app.engine(
+  `hbs`,
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: `index.hbs`,
+    layoutsDir: __dirname + "/views/layout",
+    partialsdIR: __dirname + "/views/partials",
+  })
+);
+
+app.get(`/`, (req, res) => {
+  res.render(`index`, { title: `Hello there` });
 });
+
+function mostrarProductos() {
+  const productos = [];
+}
+app.get(`/`, (req, res) => {
+  res.render(`main`, {
+    allProducts: mostrarProductos(),
+    listaProductos: true,
+  });
+});
+
+// FIN DE HANDLEBARS
+// EJS
+app.set(`view engine`, `ejs`);
+
+app.get(`/`, (req, res) => {
+  res.render(`index.ejs`);
+});
+// FIN EJS
+
+// PUG
+app.set(`view engine`, `pug`);
+
+// FIN PUG
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
--- routes/routesNames.js
+++ routes/routesNames.js
@@ -2,44 +2,25 @@ const { Router } = require(`express`);
 const router = Router();
 const productos = [];
 
+router.get("/", (req, res) => {
+  res.render(`index`);
+});
+
+router.get(`/productos`, (req, res) => {
+  res.render(`./partials/mostrarProductos`, { productos });
+});
+
 function save(object) {
   this.countID++;
   object["id"] = this.countID;
   this.productos.push(object);
   return `El id del objeto añadido es ${this.countID}`;
 }
-
-function getById(id) {
-  let result;
-  if (this.productos !== []) {
-    result = this.productos.find((x) => x.id === id);
-    if (result === undefined) {
-      result = null;
-    }
-  } else {
-    result = "Producto no encontrado";
-  }
-  return result;
-}
-
-function deleteById(id) {
-  let result;
-  if (this.productos !== []) {
-    let newProducto = this.producto.filter((x) => x.id !== id);
-    this.productos = newProducto;
-    result = "OK";
-  } else {
-    result = `Producto no encontrado`;
-  }
-  return result;
-}
-
-function update(id, obj) {
-  const index = this.producto.findIndex((obj) => obj.id == id);
-  obj.id = this[index].id;
-  this.producto[index] = obj;
-  return obj;
-}
+router.post(`/`, async (req, res) => {
+  const body = req.body;
+  contenido.save(body);
+  res.redirect(`/productos`);
+});
 
 const verificarProducto = function (req, res, next) {
   const error = "Producto no encontrado";
@@ -55,28 +36,15 @@ const errorMiddleware = function (err, req, res, next) {
   }
   next();
 };
-
-router.get("/", (req, res) => {
-  return res.json({ productos: productos });
-});
-
-router.get("/api/productos/:id", (req, res) => {
-  let id = Number(req.params);
-  return res.json(productos.getById(id));
-});
-
-router.post(
-  "/api/productos",
-  errorMiddleware,
-  verificarProducto,
-  (req, res) => {
-    const producto = req.body;
+router.post("/productos", errorMiddleware, verificarProducto, (req, res) => {
+  const producto = req.body;
+  if (producto.length > 0) {
     productos.push(producto);
-    res.json("PRODUCTO AGREGADO CON EXITO");
+    res.redirect("/productos");
   }
-);
+});
 
-router.put("/api/productos/:id", (req, res) => {
+router.put("/productos/:id", (req, res) => {
   let obj = req.body;
   let id = Number(req.params.id);
   return res.json(productos.update(id, obj));
@@ -88,3 +56,35 @@ router.delete("/api/productos/:id", (req, res) => {
 });
 
 module.exports = router;
+
+// function getById(id) {
+//   let result;
+//   if (this.productos !== []) {
+//     result = this.productos.find((x) => x.id === id);
+//     if (result === undefined) {
+//       result = null;
+//     }
+//   } else {
+//     result = "Producto no encontrado";
+//   }
+//   return result;
+// }
+
+// function deleteById(id) {
+//   let result;
+//   if (this.productos !== []) {
+//     let newProducto = this.producto.filter((x) => x.id !== id);
+//     this.productos = newProducto;
+//     result = "OK";
+//   } else {
+//     result = `Producto no encontrado`;
+//   }
+//   return result;
+// }
+
+// function update(id, obj) {
+//   const index = this.producto.findIndex((obj) => obj.id == id);
+//   obj.id = this[index].id;
+//   this.producto[index] = obj;
+//   return obj;
+// }
